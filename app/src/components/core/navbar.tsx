4e96f0100d5ac8767d2b98127813bd03bf5d7551
Divyam Singh <divyamsingh1918@gmail.com>
2026-04-07 00:19:56 +0530

    repo-setup

--- app/src/components/core/navbar.tsx
+++ app/src/components/core/navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = () => {
+const navbar = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const page = () => {
   )
 }
 
-export default page
+export default navbar
